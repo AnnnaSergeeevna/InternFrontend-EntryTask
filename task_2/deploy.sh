627ca3547ba4d73d2ea2733cfbc7351e6d4e01d8
Aa9214057525 <anna.s.lipatova@gmail.com>
2024-06-17 13:43:42 +0200

    Prepare for deployment-2

--- task_2/deploy.sh
+++ task_2/deploy.sh
@@ -7,6 +7,6 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:<AnnnaSergeeevna>/<Tests17may2024>.git master:gh-pages
+git push -f git@github.com:AnnnaSergeeevna/Tests17may2024.git master:gh-pages
 
 cd -
